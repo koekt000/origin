8759e248b172426ac74c27224b64566895980838
koekt000 <bkibkalo@ya.ru>
2025-03-28 19:04:52 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   parser.py
    	deleted:    tasks.db

--- parser.py
+++ parser.py
@@ -20,11 +20,7 @@ def parse(id):
         number =  Column(Integer)
         task = Column(String)
         answer = Column(String)
-<<<<<<< HEAD
         explain = Column(String)
-=======
->>>>>>> 991e6033f10429f5515711b15501d00dd0d0d44a
-        
     Base.metadata.create_all(engine)
 
     patern_blocks = r'<div style="margin-bottom:25px" class="(?:right|not_right)">.*?(?=<div style="margin-bottom:25px" class="(?:right|not_right)">|<!--np-->|\Z)'
@@ -32,10 +28,7 @@ def parse(id):
     patern_tasks = r'<div align="justify" width="100%" class="pbody">(.*?)(?=Пояснение)'
     patern_numbers = r'(?<=тип ).*?(?=<)'
     patern_answers = r'(?<=Правильный ответ:).*?(?=<)'
-<<<<<<< HEAD
     explain_patern = r'(?<=Пояснение).*?(?=Ваш ответ)'
-=======
->>>>>>> 991e6033f10429f5515711b15501d00dd0d0d44a
     session = Session()
     while '' in blocks:
         blocks.remove('')
@@ -46,26 +39,17 @@ def parse(id):
         numbers = re.findall(patern_numbers, i)
         answers = re.findall(patern_answers, i)
         answers.sort()
-<<<<<<< HEAD
         explain = re.findall(explain_patern, i, re.DOTALL)
         explain[0] = re.sub(r'<[^>]+>', '', explain[0])
         explain[0]= re.sub(r'&[^;]*;', '\n', explain[0])
         print(tasks, answers, numbers, explain)
         new_task = Task(task=tasks[0], answer=answers[-1], number = int(numbers[0]), explain = explain[0])
-=======
-        print(tasks, answers, numbers)
-        new_task = Task(task=tasks[0], answer=answers[-1], number = int(numbers[0]))
->>>>>>> 991e6033f10429f5515711b15501d00dd0d0d44a
         session.add(new_task)
 
     session.commit()
     session.close()
 
-<<<<<<< HEAD
 for i in range(1000000, 1001000):
-=======
-for i in range(1000000, 9999999):
->>>>>>> 991e6033f10429f5515711b15501d00dd0d0d44a
     try:
         parse('8'+str(i))
     except:
